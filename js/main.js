f42ce6d07bc3fc48b4956d1738bd132891ebea3b
katitusi <97524302+katitusi@users.noreply.github.com>
2022-05-29 21:22:23 +0200

    test
    erste

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+//Burgermenü
+
+function expand(){
+    document.querySelector(".mainnavigation").classList.toggle("main-navigation-expanded");
+}
+document.querySelector(".burgermenu").addEventListener("click",expand);
+
+mybutton = document.getElementById("myBtn");
+
+
+//Back to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+
+var mybutton = document.getElementById("myBtnTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
